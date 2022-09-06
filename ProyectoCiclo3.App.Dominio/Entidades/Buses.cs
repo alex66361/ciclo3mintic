c6426d791e0f2815c9fb639378a0f672b1f0a6df
alex66361 <map066361545@gmail.com>
2022-09-05 22:36:15 -0500

    Fir st commit

---
+++ ProyectoCiclo3.App.Dominio/Entidades/Buses.cs
@@ -0,0 +1,13 @@
+using System;
+namespace ProyectoCiclo3.App.Dominio{
+    public class Buses{
+
+        public int id {get;set;} 
+        public string marca {get;set;}            
+        public string modelo {get;set;}            
+        public float kilometraje {get;set;}      
+        public int numer_Asientos {get;set;}      
+        public string placa {get;set;}                  
+           
+    }
+}
